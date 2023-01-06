64f54f402ab00e5b3849cbe187e5009e491e8565
Yishak Kidanu <yisak04@gmail.com>
2023-01-06 16:45:40 +0000

    A function that returns an array of ids using map method

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(a) {
   if (Array.isArray(a) == false) {
-    return (newt = a.map((item) => item.id));
+    return a.map((item) => item.id);
   }
   return [];
 }
