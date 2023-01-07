ee08912ed4c68820e16e6ab311a9583f552243f6
Yishak Kidanu <yisak04@gmail.com>
2023-01-07 15:45:41 +0000

    Return number of Arguments

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let TotalArgs = 0;
-  let arg;
-  for (arg in args) {
-    TotalArgs += 1;
-  }
-  return TotalArgs;
+  return args.length;
 }
